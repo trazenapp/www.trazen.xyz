bb0721a6d6c745109e7b37f3df3abca2acd1099d
Joseph Unomieta <lionelunomieta@gmail.com>
2025-11-03 00:50:31 +0100

    fix: ensure WalletConnect Project ID is validated before use

--- utils/clientProvider.tsx
+++ utils/clientProvider.tsx
@@ -9,11 +9,15 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
 import { Toaster } from "react-hot-toast";
 
-const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string;
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID; 
+
+if (!projectId) {
+  throw new Error("WalletConnect Project ID is required and must be set as NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID in your deployment environment!");
+}
 
 const config = getDefaultConfig({
   appName: "Trazen2.0",
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
+  projectId: projectId as string,
   chains: [mainnet, polygon, optimism, arbitrum, base],
   ssr: true,
 });
